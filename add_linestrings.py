244440fa07c51e9dd1ac548181a50bab9f857fd2
Matt Westcott <matt@west.co.tt>
2012-10-29 01:46:19 +0000

    add add_linestrings script

---
+++ add_linestrings.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=traingraph_eu user=postgres")
+paths_qry = conn.cursor()
+cur = conn.cursor()
+
+paths_qry.execute("""
+	SELECT id FROM paths WHERE linestring IS NULL
+""")
+for (path_id,) in paths_qry:
+	print path_id
+	cur.execute("""
+		UPDATE paths
+		SET linestring = (
+			SELECT
+				st_makeline(st_accum(geom))
+			FROM (
+				SELECT nodes.geom
+				FROM path_nodes
+				INNER JOIN nodes ON (path_nodes.node_id = nodes.id)
+				WHERE path_nodes.path_id = %s
+				ORDER BY path_nodes.sequence_id
+			) AS foo
+		) WHERE id = %s
+	""", (path_id, path_id))
+	conn.commit()
+
+print "all paths annotated with linestrings. Adding lengths..."
+cur.execute("""
+	UPDATE paths
+	SET length = ST_Length(ST_GeogFromWKB(linestring))
+	WHERE length IS NULL AND linestring IS NOT NULL
+""")
+conn.commit()
+print "done."
+
+paths_qry.close()
+cur.close()
+conn.close()
